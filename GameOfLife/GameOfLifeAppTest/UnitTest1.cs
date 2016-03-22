6faaba238b2ff7b4531058c499fcde6e2a51fdd3
gvasko79 <gabor.vaskoszedlar@gmail.com>
2016-03-22 05:43:22 +0100

    Add initial project structure

---
+++ GameOfLife/GameOfLifeAppTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GameOfLifeAppTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
